5793d599d308e8d1cd13cd37cb3dbde0bc14e7f8
Bruce Mubangwa <40401943+brucetruth@users.noreply.github.com>
2021-05-16 12:03:54 +0200

    Finance endpoint

---
+++ src/Service/Finance/Currency_Converter_Service.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Created by Bruce Mubangwa on 19 /Apr, 2021 @ 15:00
+ */
+
+namespace Litebase\Service\Finance;
+
+
+use Litebase\Common\Http\Exception;
+use Litebase\LitebaseClient;
+use Litebase\LitebaseService;
+
+class Currency_Converter_Service extends LitebaseService
+{
+
+
+    public function __construct(LitebaseClient $client)
+    {
+        parent::__construct($client);
+        $this->endpoint = '/api/finance/converter';
+        $this->rootUrl = 'http://brooshost:8081/cloud';
+        $this->version = 'v1';
+        $this->oldData = array();
+    }
+
+    /**
+     * @param $text
+     * @return $this
+     */
+    public function setFromCurrency($text): Currency_Converter_Service
+    {
+        $this->oldData['from'] = $text;
+
+        return $this;
+    }
+
+    /**
+     * @param $text
+     * @return $this
+     */
+    public function setToCurrency($text): Currency_Converter_Service
+    {
+        $this->oldData['to'] = $text;
+
+        return $this;
+    }
+
+    /**
+     * @param $text
+     * @return $this
+     */
+    public function setToUserCurrency(): Currency_Converter_Service
+    {
+        $this->oldData['to'] = 'zmw';
+
+        return $this;
+    }
+
+    /**
+     * @param $size
+     * @return $this
+     */
+    public function setAmount($size): Currency_Converter_Service
+    {
+        $this->oldData['amount'] = $size;
+
+        return $this;
+    }
+
+
+    /**
+     * @return mixed
+     * @throws \JsonException
+     */
+    public function getCurrency(): ?string
+    {
+        if ($this->hasExected) {
+
+            if ($this->newData->status === true) {
+                return $this->newData->data->currency;
+            }
+
+        }
+        $this->_execute();
+        return $this->getCurrency();
+    }
+
+    /**
+     * @return mixed
+     * @throws \JsonException
+     */
+    public function getAmount(): ?string
+    {
+        if ($this->hasExected) {
+
+            if ($this->newData->status === true) {
+                return $this->newData->data->amount;
+            }
+
+        }
+        $this->_execute();
+        return $this->getAmount();
+    }
+
+
+    private function _execute(): void
+    {
+        if (isset($this->oldData['from'])) {
+            $this->execute($this->oldData);
+        } else {
+            throw new Exception('Currency converter amount not set.');
+        }
+    }
+
+
+}
